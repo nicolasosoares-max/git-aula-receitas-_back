68fdd4105d436396290356cdf010c88b7a720d45
Aluno SENAI <Aluno@SN-SEMPAT50205.edu.fiesc.com.br>
2026-03-03 12:37:31 -0300

    PA

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-import Fastify from 'fastify'
-
+import Fastify from 'fastify';
+import { Pool } from 'pg';
 
 const servidor = Fastify();
 
+const sql = new Pool({
+    user: "postgres",
+    password: "senai",
+    host: "localhost",
+    port: 5432,
+    database: "receitas"
+})
 
 servidor.get('/usuarios', () => {
     return 'Funcionando!'
 })
 
+servidor.post('/usuarios',async (request, reply) =>{
+    const body = request.body;
+
+    const resultado = await   sql.query('select * from usuario')
+
+    return resultado.rows
+})
 
 servidor.listen({
     port: 3000
