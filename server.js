572e033a1732db291158e857755ecd11d48983ee
Aluno SENAI <Aluno@SN-SEMPAT50205.edu.fiesc.com.br>
2026-03-05 12:26:00 -0300

    aula05/03

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-import Fastify from 'fastify';
-import { Pool } from 'pg';
+import Fastify from 'fastify'
+import { Pool } from 'pg'
 
-const servidor = Fastify();
 
 const sql = new Pool({
     user: "postgres",
@@ -11,20 +10,52 @@ const sql = new Pool({
     database: "receitas"
 })
 
+
+const servidor = Fastify();
+
+
 servidor.get('/usuarios', async () => {
     const resultado = await sql.query('select * from usuario')
     return resultado.rows
 })
 
 
-servidor.post('/usuarios',async (request, reply) =>{
+servidor.post('/usuarios', async (request, reply) => {
     const body = request.body;
 
-    const resultado = await   sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)', [body.nome, body.senha])
-
-    return 'usuario cadastrado'
+    if(!body || !body.nome || !body.senha){
+        return reply.status(400).send({
+            message:"nome e senha são obrigatorios"
+        })
+    }
+    const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)', [body.nome, body.senha])          
+    return 'Usuário Cadastrado!'
 })
 
-servidor.listen({
+
+servidor.put('/usuarios/:id', async (request, reply) => {
+    const body = request.body;
+
+     if(!body || !body.nome || !body.senha){
+        return reply.status(400).send({
+            message:"obrigatorio ter iD"
+        })
+    }
+    const id = request.params.id;
+    const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2 WHERE id = $3', [body.nome, body.senha, id])      
+    return 'Usuário Alterado!'  
+               
+})
+
+servidor.delete('/usuarios/:id', async (request, reply) => {
+
+    const id = request.params.id;
+    const resultado = await sql.query('DELETE FROM usuario where id = $1',[id]);
+    reply.status(200).send({Mmessage:'Usuário Deletado'})
+    console.log(resultado)
+
+});
+
+servidor.listen({   
     port: 3000
 })
